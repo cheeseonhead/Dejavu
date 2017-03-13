2f368112d1f76943ce000a96d4b58cdb2be9b549
Jeff Woo <cheeseonhead@gmail.com>
2017-03-12 20:45:27 -0400

    Added thumbnail property to the post annotattion

--- GiftLog.playground/Sources/Tab2/PostAnnotation.swift
+++ GiftLog.playground/Sources/Tab2/PostAnnotation.swift
@@ -6,11 +6,13 @@ class PostAnnotation: NSObject, MKAnnotation
     var coordinate: CLLocationCoordinate2D
     var title: String?
     var subtitle: String?
+    var thumbNail: UIImage?
     
-    init(coordinate: CLLocationCoordinate2D, title: String, subtitle: String)
+    init(coordinate: CLLocationCoordinate2D, title: String, subtitle: String, thumbNail: UIImage)
     {
         self.coordinate = coordinate
         self.title = title
         self.subtitle = subtitle
+        self.thumbNail = thumbNail
     }
 }
