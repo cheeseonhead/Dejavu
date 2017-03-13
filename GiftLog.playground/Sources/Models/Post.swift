75af8e0dd7f01299bf9960504d0e4c9b729d8c66
Jeff Woo <cheeseonhead@gmail.com>
2017-03-12 21:47:29 -0400

    Added formatter and date property

--- GiftLog.playground/Sources/Models/Post.swift
+++ GiftLog.playground/Sources/Models/Post.swift
@@ -3,8 +3,15 @@ import MapKit
 
 struct Post
 {
+    static let dateFormatter = { () -> DateFormatter in
+        var temp = DateFormatter()
+        temp.dateFormat = "MM-dd-yyyy HH:mm"
+        return temp
+    }()
+    
     var title: String
     var description: String
+    var date: Date
     var image: UIImage?
     var location: CLLocationCoordinate2D
 }
