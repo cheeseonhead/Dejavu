ae922d004e12b6469bd4000545eee6cb9a128cc9
Jeff Woo <cheeseonhead@gmail.com>
2017-03-14 23:00:23 -0400

    Added the post card view

---
+++ GiftLog.playground/Sources/Tab1/ViewPostVC/ViewPostCardView.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ViewPostCardView: UIView
+{
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = #colorLiteral(red: 0.3803921569, green: 0.7529411765, blue: 0.5725490196, alpha: 1)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
