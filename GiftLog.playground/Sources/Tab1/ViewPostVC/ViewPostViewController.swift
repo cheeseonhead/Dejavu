ba3badff1f28ff0382a18a330c6e8740c4d45a71
Jeff Woo <cheeseonhead@gmail.com>
2017-03-14 23:06:12 -0400

    Added scrollview to the post vc

--- GiftLog.playground/Sources/Tab1/ViewPostVC/ViewPostViewController.swift
+++ GiftLog.playground/Sources/Tab1/ViewPostVC/ViewPostViewController.swift
@@ -2,10 +2,16 @@ import UIKit
 
 class ViewPostViewController: UIViewController
 {
+    var scrollView = UIScrollView(frame: CGRect.zero)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Post"
         view.backgroundColor = UIColor.brown
+        
+        scrollView.frame = view.bounds
+        scrollView.backgroundColor = UIColor.black
+        view.addSubview(scrollView)
     }
 }
