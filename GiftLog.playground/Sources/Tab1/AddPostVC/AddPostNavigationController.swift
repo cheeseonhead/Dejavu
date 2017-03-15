efa6af5c2b8df7f97ed86c7c9557ad5191da86aa
Jeffrey Wu <jeffrey@tophat.com>
2017-03-15 19:05:12 -0400

    Initialize it

--- GiftLog.playground/Sources/Tab1/AddPostVC/AddPostNavigationController.swift
+++ GiftLog.playground/Sources/Tab1/AddPostVC/AddPostNavigationController.swift
@@ -1,2 +1,20 @@
-import Foundation
+import UIKit
 
+class AddPostNavigationController: UINavigationController
+{
+    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
+    {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        self.pushViewController(AddPostViewController(), animated: false)
+        
+        navigationBar.barStyle = .blackOpaque
+        navigationBar.barTintColor = #colorLiteral(red: 0.4588235294, green: 0.3960784314, blue: 0.5254901961, alpha: 1)
+        navigationBar.tintColor = UIColor.white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
