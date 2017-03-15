25f74a6ef70540efd9fe2458bf263a820fb6adbb
Jeffrey Wu <jeffrey@tophat.com>
2017-03-15 18:50:03 -0400

    Some simple setup

--- GiftLog.playground/Sources/Tab1/AddPostVC/AddPostViewController.swift
+++ GiftLog.playground/Sources/Tab1/AddPostVC/AddPostViewController.swift
@@ -1,2 +1,11 @@
-import Foundation
+import UIKit
 
+class AddPostViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        navigationItem.title = "Add Post"
+        view.backgroundColor = UIColor.cyan
+    }
+}
