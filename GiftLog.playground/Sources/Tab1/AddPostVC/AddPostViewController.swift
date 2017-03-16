47c8c7410f038ba90f345799563a03004ec5b37d
Jeffrey Wu <jeffrey@tophat.com>
2017-03-16 13:19:59 -0400

    Got a brown view in there

--- GiftLog.playground/Sources/Tab1/AddPostVC/AddPostViewController.swift
+++ GiftLog.playground/Sources/Tab1/AddPostVC/AddPostViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class AddPostViewController: UIViewController
 {
+    var groupView = UIView()
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -10,6 +12,19 @@ class AddPostViewController: UIViewController
         
         print(navigationItem)
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonPressed))
+        
+        groupView.backgroundColor = UIColor.brown
+        groupView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(groupView)
+        
+        let constraints = [
+            NSLayoutConstraint(item: groupView, attribute: .top, relatedBy: .equal, toItem: topLayoutGuide, attribute: .bottom, multiplier: 1.0, constant: 10),
+            NSLayoutConstraint(item: groupView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottomMargin, multiplier: 1.0, constant: -10),
+            NSLayoutConstraint(item: groupView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1.0, constant: 10),
+            NSLayoutConstraint(item: groupView, attribute: .right, relatedBy: .equal, toItem: view, attribute: .right, multiplier: 1.0, constant: -10),
+        ]
+        
+        view.addConstraints(constraints)
     }
     
     func cancelButtonPressed()
