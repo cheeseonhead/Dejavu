5b622efe40c4aa81202ee59316f708c090c14d24
Jeff Woo <cheeseonhead@gmail.com>
2017-03-21 02:45:50 -0400

    Added the blur imageview to the card

--- GiftLog.playground/Sources/Tab1/ViewPostVC/View Post Card/ViewPostCardView.swift
+++ GiftLog.playground/Sources/Tab1/ViewPostVC/View Post Card/ViewPostCardView.swift
@@ -2,13 +2,43 @@ import UIKit
 
 class ViewPostCardView: CardView
 {
-    var blurImage = UIImageView()
+    var contentView = ViewPostCardContentView()
     
     required init() {
         super.init()
         
+        setupContentView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupContentView()
+    {
+        addSubview(contentView)
+        
+        let constraints = [
+            NSLayoutConstraint(item: contentView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: contentView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: contentView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: contentView, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: 0),
+        ]
+        
+        addConstraints(constraints)
+    }
+}
+
+class ViewPostCardContentView: UIView
+{
+    var blurImage = BlurImage()
+    
+    required init() {
+        super.init(frame: CGRect.zero)
+        
         setupSelf()
         setupBlurImage()
+        setupTemp()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -17,6 +47,90 @@ class ViewPostCardView: CardView
     
     func setupSelf()
     {
+        clipsToBounds = true
+        translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        layer.cornerRadius = 10
+    }
+    
+    func setupBlurImage()
+    {
+        addSubview(blurImage)
+        
+        let constraints = [
+            NSLayoutConstraint(item: blurImage, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: blurImage, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: blurImage, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: blurImage, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 150)
+        ]
+        
+        addConstraints(constraints)
+    }
+    
+    func setupTemp()
+    {
+        blurImage.image = UIImage(named: ImageName.winter.rawValue)
+        addConstraint(NSLayoutConstraint(item: blurImage, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0))
+    }
+}
+
+class BlurImage: UIView
+{
+    var imageView = UIImageView()
+    
+    var image: UIImage? {
+        didSet {
+            imageView.image = image
+        }
+    }
+    
+    init() {
+        super.init(frame: CGRect.zero)
+        
+        setupSelf()
+        setupImageView()
+        setupBlur()
+    }
+    
+    func setupSelf()
+    {
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func setupBlur()
+    {
+        let darkBlur = UIBlurEffect(style: .regular)
+        let blurView = UIVisualEffectView(effect: darkBlur)
+        blurView.alpha = 0.8
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(blurView)
+        
+        let constraints = [
+            NSLayoutConstraint(item: blurView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: blurView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: blurView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: blurView, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: 0),
+            ]
+        
+        addConstraints(constraints)
+    }
+    
+    func setupImageView()
+    {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(imageView)
+        
+        let constraints = [
+            NSLayoutConstraint(item: imageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: imageView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: imageView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: imageView, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: 0),
+            ]
+        
+        addConstraints(constraints)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
