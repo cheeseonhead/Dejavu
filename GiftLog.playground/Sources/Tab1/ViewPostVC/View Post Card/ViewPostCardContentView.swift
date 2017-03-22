4191e2b61c060b99e570259fbb2450de8b2e7715
Jeff Woo <cheeseonhead@gmail.com>
2017-03-22 01:36:36 -0400

    Added a thumbnail view that looks pretty

--- GiftLog.playground/Sources/Tab1/ViewPostVC/View Post Card/ViewPostCardContentView.swift
+++ GiftLog.playground/Sources/Tab1/ViewPostVC/View Post Card/ViewPostCardContentView.swift
@@ -2,13 +2,22 @@ import UIKit
 
 class ViewPostCardContentView: UIView
 {
+    fileprivate struct Style
+    {
+        static let blurImageHeight: CGFloat = 150
+        static let thumbNailTopPadding: CGFloat = 20
+        static let thumbNailSize: CGFloat = 200
+    }
+    
     var blurImage = BlurImage()
+    var thumbNailView = ThumbNailView()
     
     required init() {
         super.init(frame: CGRect.zero)
         
         setupSelf()
         setupBlurImage()
+        setupThumbNail()
         setupTemp()
     }
     
@@ -35,7 +44,7 @@ class ViewPostCardContentView: UIView
             NSLayoutConstraint(item: maskView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 0),
             NSLayoutConstraint(item: maskView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0),
             NSLayoutConstraint(item: maskView, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: 0),
-            NSLayoutConstraint(item: maskView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 150)
+            NSLayoutConstraint(item: maskView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: Style.blurImageHeight)
         ]
         
         addConstraints(constraints)
@@ -51,9 +60,75 @@ class ViewPostCardContentView: UIView
         maskView.addConstraints(constraints)
     }
     
+    func setupThumbNail()
+    {
+        addSubview(thumbNailView)
+        
+        let constraints = [
+            NSLayoutConstraint(item: thumbNailView, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: thumbNailView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: Style.thumbNailTopPadding),
+            NSLayoutConstraint(item: thumbNailView, attribute: .width, relatedBy: .equal, toItem: thumbNailView, attribute: .height, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: thumbNailView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: Style.thumbNailSize)
+        ]
+        
+        addConstraints(constraints)
+    }
+    
     func setupTemp()
     {
+        addConstraint(NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 1000))
         blurImage.image = UIImage(named: ImageName.winter.rawValue)
-        addConstraint(NSLayoutConstraint(item: blurImage, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0))
+        thumbNailView.image = UIImage(named: ImageName.winter.rawValue)
+    }
+}
+
+class ThumbNailView: UIView
+{
+    fileprivate struct Style
+    {
+        static let cornerRaius: CGFloat = 4
+        static let imagePadding: CGFloat = 5
+    }
+    
+    var imageView = UIImageView()
+    
+    var image: UIImage? {
+        didSet {
+            imageView.image = image
+        }
+    }
+    
+    required init()
+    {
+        super.init(frame: CGRect.zero)
+        
+        setupSelf()
+        setupImageView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupSelf()
+    {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = Style.cornerRaius
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    
+    func setupImageView()
+    {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(imageView)
+        
+        let constraints = [
+            NSLayoutConstraint(item: imageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: Style.imagePadding),
+            NSLayoutConstraint(item: imageView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: -Style.imagePadding),
+            NSLayoutConstraint(item: imageView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: Style.imagePadding),
+            NSLayoutConstraint(item: imageView, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: -Style.imagePadding),
+        ]
+        
+        addConstraints(constraints)
     }
 }
