adba47c27ff8a82fb63f497af6a425ff3130e6d9
Jeff Woo <cheeseonhead@gmail.com>
2017-03-26 16:44:35 -0400

    Typo?

--- Dejavu.playground/Pages/View.xcplaygroundpage/Contents.swift
+++ Dejavu.playground/Pages/View.xcplaygroundpage/Contents.swift
@@ -13,7 +13,7 @@ import PlaygroundSupport
  
  On the right hand side you can see a list of your **Déjà vu**s, including their *Title*, *Date*, *Description*, and *ThumbNail*. They are sorted by the distance to your current location from the closest to furthest. (You can try changing the parameter on the bottom of the page to mimic different current locations)
  
- Imagine a situation which you got that **Déjà vu** feeling. You immediately open the app, and the first thing you would do is skim over the first few posts to see if any of their *title/description/thumbnail* resembles your current situation. Hopefully they don't. If they don't, you can go to the top right and add an entry ([*see adding entry*](Adding)). You can be sure you broke the **Déjà vu** now, because if you did the same thing last time, an entry would already be in the list.
+ Imagine a situation which you got that **Déjà vu** feeling. You immediately open the app, and the first thing you would do is skim over the first few posts to see if any of their *title/description/thumbnail* resembles your current situation. Hopefully they don't. If they don't, you can go to the top right and add an entry ([*see adding entry*](AddEntry)). You can be sure you broke the **Déjà vu** now, because if you did the same thing last time, an entry would already be in the list.
  
  Viewing an entry is as easy as tapping on one. It will take you to a scene with beautiful card design, and all the information about the **Déjà vu** is there ready for you, including the previously omitted: *location*.
  
