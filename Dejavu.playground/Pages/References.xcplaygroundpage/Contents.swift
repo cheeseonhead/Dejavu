06f6f3807d658320328ef7800e4642c6d47c22bf
Jeffrey Wu <jeffrey@tophat.com>
2017-03-26 16:40:21 -0400

    idk

---
+++ Dejavu.playground/Pages/References.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+/*:
+ ****
+ [Table of Contents](TableOfContents) | [Previous](@previous) | [Next](@next)
+ ****
+ # References
+ - callout(Blur Image): [StackOverflow](http://stackoverflow.com/questions/30953201/adding-blur-effect-to-background-in-swift)
+ 
+ 
+ - callout(ScrollView): Special thanks to Gene Goykman for teaching me how AutoLayout works on scroll views. It was essential to create the cards.
+ 
+    [Website](http://goykhman.ca/gene/)
+ 
+ 
+ - callout(Collection View): Special thanks to the blog I look at a lot **raywenderlich**. The post on collection view was a big help.
+    
+    [raywenderlich](https://www.raywenderlich.com/136159/uicollectionview-tutorial-getting-started)
+ 
+ 
+ - callout(Apple Developer Team): Thanks to Apple for being so dedicated to creating the best language, and providing all these tools for us to use. Thanks for loving developers!
+ */
+ 
