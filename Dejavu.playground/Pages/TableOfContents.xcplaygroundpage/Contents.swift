f6f0c75cb6b779681e922749d69530d1947e2972
Jeff Woo <cheeseonhead@gmail.com>
2017-03-26 22:06:38 -0400

    Deleted some text to make it lighter

--- Dejavu.playground/Pages/TableOfContents.xcplaygroundpage/Contents.swift
+++ Dejavu.playground/Pages/TableOfContents.xcplaygroundpage/Contents.swift
@@ -4,19 +4,14 @@
     1. Sometimes the timeline on the right hand side doesn't correctly display the simulator, a restart to XCode is usually required.
     2. Sometimes the map views don't display the names of locations, only displays the landscape. It doesn't affect the demonstration of the app. A re-run solves the problem 60% of the time.
  
- Have you ever been in a situation where you suddenly feel like you have dont this before? That's a creepy feeling we call **Déjà vu**. It's a strange physical sensation written off by science as a meaningless and explainable biological phenomena centered in the brain. But for people that don't totally believe in that, usually try to break the **Déjà vu** by doing something different. It could be screaming out a weird phrase, or dancing some weird dance moves.
- 
- But how can you make sure you actually broke the **Déjà vu**?
- 
- The answer is: *You can't*
- 
- That's why this app was created, it's here to make sure you don't fall into an endless loop of time.
+ This app was created for situations where you feel like you have experienced this before, but you're not sure how to break the **Déjà vu**.
  
  On each page of the playground, you will learn about one significant function of the app.
  
  - Viewing Entries
     - [View by List](View)
     - [View by Map](ViewMap)
+ - [Alert Popup](Popup)
  - [Adding Entries](AddEntry)
  ****
  [Next](@next)
--- Dejavu.playground/Pages/View.xcplaygroundpage/Contents.swift
+++ Dejavu.playground/Pages/View.xcplaygroundpage/Contents.swift
@@ -11,9 +11,7 @@ import PlaygroundSupport
  
  The app is centered around your **Déjà vu**s, so the most important function is to allow the user to view them, otherwise how are you going to see if it already happened before?
  
- On the right hand side you can see a list of your **Déjà vu**s, including their *Title*, *Date*, *Description*, and *ThumbNail*. They are sorted by the distance to your current location from the closest to furthest. (You can try changing the parameter on the bottom of the page to mimic different current locations)
- 
- Imagine a situation which you got that **Déjà vu** feeling. You immediately open the app, and the first thing you would do is skim over the first few posts to see if any of their *title/description/thumbnail* resembles your current situation. Hopefully they don't. If they don't, you can go to the top right and add an entry ([*see adding entry*](AddEntry)). You can be sure you broke the **Déjà vu** now, because if you did the same thing last time, an entry would already be in the list.
+ On the right hand side you can see a list of your **Déjà vu**s, including their *Title*, *Date*, *Description*, and *ThumbNail*. They are sorted by the distance to your current location from the closest to furthest.
  
  Viewing an entry is as easy as tapping on one. It will take you to a scene with beautiful card design, and all the information about the **Déjà vu** is there ready for you, including the previously omitted: *location*.
  
